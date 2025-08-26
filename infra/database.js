b6f97849e084b07e4ffe8b970bd7b8376cc6d71d
thiago <thiagoapcam@gmail.com>
2025-08-25 23:58:08 -0300

    feat:cria conexao com banco de dados

---
+++ infra/database.js
@@ -0,0 +1,29 @@
+import { Client } from "pg";
+
+async function getNewClient() {
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port:process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database:process.env.POSTGRES_DB,
+        password:process.env.POSTGRES_PASSWORD,
+
+
+    })
+    await client.connect()
+    return client
+}
+async function query(queryobject){
+    let client
+
+    try{
+        client = await getNewClient()
+        const result = await client.query(queryobject)
+        return result
+    } catch(error){
+        console.error(error)
+        throw error
+    } finally{client.end()}
+    
+}
+export default{query,getNewClient}
