b4aad46d4687f1c57a724b3257a48f481b7084b6
Thalys <tgbr66@gmail.com>
2025-09-02 16:28:47 -0300

    fix: corrige o texto dos testes

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,7 +6,7 @@ async function cleanDatabase() {
     await database.query("drop schema public cascade; create schema public;")
 }
 
-test("Get no endpoint /api/v1/migrations deve retornar 200", async () => {
+test("GET no endpoint /api/v1/migrations deve retornar 200", async () => {
     const response = await fetch("http:/localhost:3000/api/v1/migrations")
     expect(response.status).toBe(200)
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("Get no endpoint /api/v1/status/ deve retornar 200", async () => {
+test("GET no endpoint /api/v1/status/ deve retornar 200", async () => {
     const response = await fetch("http:/localhost:3000/api/v1/status")
     expect(response.status).toBe(200)
 
