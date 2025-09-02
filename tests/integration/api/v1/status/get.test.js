b7cde93b2647604e0c3b5bc4d3ded7672a2d2759
Thalys <tgbr66@gmail.com>
2025-09-02 15:39:27 -0300

    test: adiciona o teste para o endpoint get /api/v1/status

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,15 @@
+test("Get no endpoint /api/v1/status/ deve retornar 200", async () => {
+    const response = await fetch("http:/localhost:3000/api/v1/status")
+    expect(response.status).toBe(200)
+
+    const responseBody = await response.json()
+    const parsedUpdateAt = new Date(responseBody.updated_at).toISOString()
+
+    expect(responseBody.updated_at).toEqual(parsedUpdateAt)
+
+    expect(responseBody.dependencies.database.version).toEqual("16.0")
+
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+})
