d03af511b85d1b13642e6498356b99fe24e3b486
Thalys Rocha Rodrigues <tgbr66@gmail.com>
2025-09-09 20:24:21 -0300

    feat: cria um script para garantir que o banco de dados esteja aceitando conexões localmente (#9)

---
+++ infra/scripts/wait-for-database.js
@@ -0,0 +1,17 @@
+const { exec } = require("node:child_process")
+
+function checkPostgres() {
+  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn)
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".")
+      checkPostgres()
+      return
+    }
+    console.log("\n🟢 O banco de dados está pronto e aceitando conexões")
+  }
+}
+
+process.stdout.write("🔴 Aguardando banco de dados ficar pronto conexões")
+checkPostgres()
