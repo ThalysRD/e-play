e1e79e41f3eae18f9a76092e4108c65158740d7f
thiago <thiagoapcam@gmail.com>
2025-09-09 21:32:10 -0300

    feat:cria o estado inicial da model user

--- infra/scripts/wait-for-database.js
+++ infra/scripts/wait-for-database.js
@@ -1,17 +1,17 @@
-const { exec } = require("node:child_process")
+const { exec } = require("node:child_process");
 
 function checkPostgres() {
-  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn)
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-      process.stdout.write(".")
-      checkPostgres()
-      return
+      process.stdout.write(".");
+      checkPostgres();
+      return;
     }
-    console.log("\n🟢 O banco de dados está pronto e aceitando conexões")
+    console.log("\n🟢 O banco de dados está pronto e aceitando conexões");
   }
 }
 
-process.stdout.write("🔴 Aguardando banco de dados ficar pronto conexões")
-checkPostgres()
+process.stdout.write("🔴 Aguardando banco de dados ficar pronto conexões");
+checkPostgres();
