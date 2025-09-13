61087f70aed2d031061d3222a1715bf36a651bd0
Thalys <tgbr66@gmail.com>
2025-09-13 16:47:34 -0300

    feat: cria o controller

---
+++ infra/controller.js
@@ -0,0 +1,31 @@
+import {
+  InternalServerError,
+  MethodNotAllowedError,
+  ServiceError
+} from "infra/errors";
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+
+  console.error(publicErrorObject);
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+
+
+
+const controller = {
+  errorHandlers: {
+    onNoMatch: onNoMatchHandler,
+    onError: onErrorHandler,
+  },
+};
+
+export default controller;
