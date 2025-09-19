2b13f5ceb1f023678df9dcadbe40a9ad9d3b0f61
Thalys Rocha Rodrigues <tgbr66@gmail.com>
2025-09-19 22:12:50 +0000

    refactor: remove comentário

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,12 +1,12 @@
 import orchestrator from "tests/orchestrator.js";
-import user from "models/user.js";
-import password from "models/password";
+import user from "models/user.js"
+import password from "models/password"
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices();
-  await orchestrator.clearDatabase();
-  await orchestrator.runPendingMigrations();
-});
+  await orchestrator.waitForAllServices()
+  await orchestrator.clearDatabase()
+  await orchestrator.runPendingMigrations()
+})
 
 describe("POST /api/v1/users", () => {
   describe("Usuário anônimo", () => {
@@ -21,12 +21,12 @@ describe("POST /api/v1/users", () => {
           username: "teste123",
           email: "teste@teste.com",
           password: "senha123",
-          cpf: "11144477735",
+          cpf: "11144477735"
         }),
-      });
+      })
 
-      expect(response.status).toBe(201);
-      const responseBody = await response.json();
+      expect(response.status).toBe(201)
+      const responseBody = await response.json()
 
       expect(responseBody).toEqual({
         user_id: responseBody.user_id,
@@ -40,15 +40,15 @@ describe("POST /api/v1/users", () => {
         address: null,
         zip_code: null,
         created_at: responseBody.created_at,
-        updated_at: responseBody.updated_at,
-      });
+        updated_at: responseBody.updated_at
+      })
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
 
       const userInDatabase = await user.findOneByUsername("teste123");
       const correctPasswordMatch = await password.compare(
         "senha123",
-        userInDatabase.password_hash, //
+        userInDatabase.password_hash, // 
       );
 
       const incorrectPasswordMatch = await password.compare(
@@ -60,20 +60,5 @@ describe("POST /api/v1/users", () => {
       expect(incorrectPasswordMatch).toBe(false);
     });
 
-    test("Garantir que não tenha email duplicado", async () => {
-      // const response1 = await fetch("http://localhost:3000/api/v1/users", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({
-      //     name: "Teste da Costa",
-      //     username: "thalysrd",
-      //     email: "teste2@teste.com",
-      //     password: "senha123",
-      //     cpf: "11144477735"
-      //   }),
-      // })
-    });
-  });
-});
+  })
+})
