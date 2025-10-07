4d392a0192214a5277c1bfd2a7af6757e07a09dd
Thalys Rocha Rodrigues <tgbr66@gmail.com>
2025-10-07 19:37:06 -0300

    style: tela base com nav bar, body e footer (#21)
    Co-authored-by: mari.arujjo <mariana.estudo9889@gmail.com>

---
+++ pages/configuracoes/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const ConfiguracoesPage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à tela de configurações!</h1>
+      <p>Este é o E-Play.</p>
+    </div>
+  );
+};
+
+export default ConfiguracoesPage;
