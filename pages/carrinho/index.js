56166fdacff1d5eed3d6ad888cb47c86443b0eca
mari.arujjo <mariana.estudo9889@gmail.com>
2025-10-07 19:32:39 -0300

    style: tela base com nav bar, body e footer

---
+++ pages/carrinho/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const CarrinhoPage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à tela do carrinho!</h1>
+      <p>Este é o E-Play.</p>
+    </div>
+  );
+};
+
+export default CarrinhoPage;
