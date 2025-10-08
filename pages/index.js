686809a1d0da7cbcbed12aa6ca8175f27714ff02
Mariana Araújo <120474044+mari-arujjo@users.noreply.github.com>
2025-10-08 20:27:38 -0300

    feat: iniciando redirecionamento de telas, implementação de css com paleta (#24)

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import LayoutPadrao from "components/LayoutPadrao";
+import SearchBar from "components/SearchBar";
+import styles from "styles/home.module.css";
 
 export default function HomePage() {
   return (
     <LayoutPadrao>
+      <header className={styles.header}>
+        <SearchBar />
+      </header>
+
       <div>
         <h1>Bem-vindo à tela inicial</h1>
         <p>Este é o E-Play.</p>
       </div>
+
     </LayoutPadrao>
   );
 }
