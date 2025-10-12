a1a0bae0c444989cbaab9b77e2abe457d4c0dfb5
mari.arujjo <mariana.estudo9889@gmail.com>
2025-10-12 10:51:10 -0300

    fix: corrigindo o bug de renderização

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import LayoutPadrao from "components/LayoutPadrao";
-
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const noLayoutRoutes = ["/login", "/cadastro"];
+
+  if (noLayoutRoutes.includes(router.pathname)) {
+    return <Component {...pageProps} />;
+  }
   return (
     <LayoutPadrao>
       <Component {...pageProps} />
