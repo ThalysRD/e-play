cf21db25584785b792b594042d1c46809070138b
Thalys <tgbr66@gmail.com>
2025-10-16 20:53:46 -0300

    feat: adiciona a função de criar e listar anúncios

--- infra/migrations/1757273523128_create-categories.js
+++ infra/migrations/1757273523128_create-categories.js
@@ -4,8 +4,27 @@ exports.up = (pgm) => {
     name: {
       type: "varchar(255)",
       notNull: true,
+      unique: true,
     },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    }
   });
+
+  pgm.sql(`
+    INSERT INTO categories (name) VALUES
+    ('Ação'),
+    ('Aventura'),
+    ('Estratégia'),
+    ('RPG'),
+    ('Esportes'),
+    ('Simulação'),
+    ('Consoles'),
+    ('Acessórios'),
+    ('Colecionáveis');
+  `);
 };
 
 exports.down = (pgm) => {
