d2bfb28b6b58780b149976c35db27f367948ff33
Thalys <tgbr66@gmail.com>
2025-10-16 21:00:52 -0300

    fix: corrige o footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,23 +1,16 @@
 import styles from "styles/componentes/Footer.module.css";
-import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <Link href="/" className={styles.homeLink}>
-          ← Voltar para a página inicial
-        </Link>
-
-        <div className={styles.legalLinksContainer}>
-          <a href="/termos-de-uso" className={styles.legalLink}>
-            Termos de Uso
-          </a>
-          <span className={styles.linkSeparator}>|</span>
-          <a href="/politicas-de-privacidade" className={styles.legalLink}>
-            Políticas de Privacidade
-          </a>
-        </div>
+      <div className={styles.legalLinksContainer}>
+        <a href="/termos-de-uso" className={styles.legalLink}>
+          Termos de Uso
+        </a>
+        <span className={styles.linkSeparator}>|</span>
+        <a href="/politicas-de-privacidade" className={styles.legalLink}>
+          Políticas de Privacidade
+        </a>
       </div>
     </footer>
   );
